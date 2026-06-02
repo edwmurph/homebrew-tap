b3766076198993a98e81b85d1adad4e496cfdbfe
Edward Murphy <20996513+edwmurph@users.noreply.github.com>
2026-06-02 15:54:34 +0000

    weft: add 0.12.1 bottle.

--- Formula/weft.rb
+++ Formula/weft.rb
@@ -6,6 +6,12 @@ class Weft < Formula
   sha256 "979d0bdb9a55b20fbb6bafb31722abb45621a9dcd4ad3a8b410add9e655511ed"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/edwmurph/homebrew-tap/releases/download/weft-0.12.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "956bc57daccc57ef69defe16be005caf3bd051b51dfe42a1273c5fa4de3f06ea"
+  end
+
   depends_on "go" => :build
 
   def install
